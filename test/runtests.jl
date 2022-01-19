c81ea9280319cfd9d0aebb200597b6b879c7bf01
Matthew Fishman <mfishman@flatironinstitute.org>
2022-01-19 15:23:02 -0500

    Add initial implementation
    Add initial implementation and tests.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,11 @@
 using CustomVertexGraphs
+using Graphs
 using Test
 
 @testset "CustomVertexGraphs.jl" begin
-    # Write your tests here.
+  g = set_vertices(grid((4,)), ["A", "B", "C", "D"])
+  @test vertices(g[["A", "B"]]) == ["A", "B"]
+  @test has_edge(g, "A" => "B")
+  add_edge!(g, "A" => "C")
+  @test has_edge(g, "A" => "C")
 end
